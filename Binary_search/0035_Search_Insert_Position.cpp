72db7f26dc13ae11a59c033d3230f06b13665ede
Mansi Singhal <singhalmansi1621@gmail.com>
2026-08-01 18:55:11 +0530

    Added LeetCode 35 - Search Insert Position

---
+++ Binary_search/0035_Search_Insert_Position.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+// LeetCode 35
+// Problem: Search Insert Position
+// Difficulty: Easy
+// Topic: Binary Search
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+      int s=0;
+      int e=nums.size()-1;
+      while(s<=e){
+        int mid=s+(e-s)/2;
+        if(nums[mid]==target)
+        return mid;
+        else if(nums[mid]>target)
+        e=mid-1;
+        else if(nums[mid]<target)
+        //search right
+        s=mid+1;
+      }  
+      return s;
+    }
+};
